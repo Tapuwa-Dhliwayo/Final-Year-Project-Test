9139e788010f425d505c30636d098c5280701359
Tapuwa-Dhliwayo <tapsdhliwayo@gmail.com>
2019-09-12 13:05:50 +0200

    Init

---
+++ build/ReceiveApplication/CMakeFiles/RecApp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RecApp.dir/RecApp.cpp.o"
+  "../bin/RecApp.pdb"
+  "../bin/RecApp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RecApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
